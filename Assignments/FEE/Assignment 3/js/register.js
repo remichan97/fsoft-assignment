845e6e379969a494a8146be70fc2f6135ea46488
Trương Vũ Bình <77116130+remichan97@users.noreply.github.com>
2022-09-10 01:00:18 +0700

    Proper validate for register form

--- Assignments/FEE/Assignment 3/js/register.js
+++ Assignments/FEE/Assignment 3/js/register.js
@@ -4,20 +4,42 @@ function validate() {
 	let repassword = document.reg.repw.value;
 	let username = document.reg.username.value;
 
+	const emailTag = document.getElementById("email");
+	const usernameTag = document.getElementById("username");
+	const passwordTag = document.getElementById("password");
+	const repasswordTag = document.getElementById("repassword");
+
 	let text;
 
 	if (!email || !password || !repassword || username) {
 		text = "This field is required";
-		document.getElementById("validateuser").innerHTML = text;
-		document.getElementById("validateemail").innerHTML = text;
-		document.getElementById("validatepw").innerHTML = text;
-		document.getElementById("validaterepw").innerHTML = text;
+		if (!email) {
+			document.getElementById("validateemail").innerHTML = text;
+			emailTag.style.borderColor = "red";
+		} else if(!ppassword) {
+			document.getElementById("validatepw").innerHTML = text;
+			passwordTag.style.borderColor = "red";
+		} else if (!repassword) {
+			document.getElementById("validaterepw").innerHTML = text;
+			repasswordTag.style.borderColor = "red";
+		} else {
+			document.getElementById("validateuser").innerHTML = text;
+			usernameTag.style.borderColor = "red";
+		}
 		return false;
 	}	
 	
 	if (password != repassword) {
 		text = "Password do not match!";
 		document.getElementById("validaterepw").innerHTML = text;
+		repasswordTag.style.borderColor = "red";
+		return false;
+	}
+
+	if (!re.test(email)) {
+		text = "Input is not an email. Enter a valid email addess";
+		document.getElementById("validation").innerHTML = text;
+		emailTag.style.borderColor = "red";
 		return false;
 	}
 
--- Assignments/FEE/Assignment 3/js/signin.js
+++ Assignments/FEE/Assignment 3/js/signin.js
@@ -1,6 +1,10 @@
 function validate() {
 	let email = document.getElementById("email").value;
 	let password = document.getElementById("password").value;
+
+	var emailTag = document.getElementById("email");
+	var passwordTag = document.getElementById("password");
+
 	const re = /\S+@\S+\.\S+/;
 
 	let text;
@@ -9,10 +13,12 @@ function validate() {
 		if (!email) {
 			text = "This field is required";
 			document.getElementById("validation").innerHTML = text;
+			passwordTag.style.borderColor = "red";
 		}
 		if (!password) {
 			text = "This field is required";
 			document.getElementById("validationpw").innerHTML = text;
+			emailTag.style.borderColor = "red";
 		}
 		return false;
 	}
@@ -20,18 +26,21 @@ function validate() {
 	if (email.length > 50) {
 		text = "Maximum length is 50 characters";
 		document.getElementById("validation").innerHTML = text;
+		emailTag.style.borderColor = "red";
 		return false;
 	}
 
 	if (!password.length > 50) {
 		text = "Maximum length is 50 characters";
 		document.getElementById("validationpw").innerHTML = text;
+		passwordTag.style.borderColor = "red";
 		return false;	
 	}
 
 	if (!re.test(email)) {
 		text = "Input is not an email. Enter a valid email addess";
 		document.getElementById("validation").innerHTML = text;
+		emailTag.style.borderColor = "red";
 		return false;
 	}
 
